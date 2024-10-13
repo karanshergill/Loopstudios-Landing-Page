13f955b53909353ae1b483891a0b3dc17d58de55
Karan <email@karanshergill.com>
2024-10-13 17:43:34 +0530

    add tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,18 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'alata': ["'Alata', 'sans-serif'"],
+        'josefin': ["'Josefin Sans Variable', 'sans-serif'"]
+      },
+      colors: {
+        primary: {
+          'white': 'hsl(0, 0%, 100%)',
+          'black': 'hsl(0, 0%, 0%)',
+          'dark-gray': 'hsl(0, 0%, 55%)',
+          'very-dark-gray': 'hsl(0, 0%, 41%)'
+        }
+      },
       backgroundImage: {
         'hero_mobile': "url('/mobile/image-hero.jpg')"
       }
