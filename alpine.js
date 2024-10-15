71a02a2afa4b524519f1bddda33c2a5237ec3d19
Karan <email@karanshergill.com>
2024-10-16 02:18:50 +0530

    add hamburger menu

--- alpine.js
+++ alpine.js
@@ -1,1 +1,14 @@
+import Alpine from 'alpinejs'
 import { navItems, creationItems } from './data.js';
+
+document.addEventListener('alpine:init', () => {
+    console.log('Alpine is initialized');
+
+    Alpine.data('alpineData', () => ({
+        navItems,
+        isOpen: false,
+        creationItems
+    }));
+});
+
+Alpine.start();
--- data.js
+++ data.js
@@ -17,48 +17,48 @@ const navItems = [
     },
     {
         "label": "Support",
-        "link": "Support"
+        "link": "support"
     }
 ]
 
 const creationItems = [
     {
-        "name": "Deep Earth",
+        "label": "Deep Earth",
         "image-small": "/mobile/image-deep-earth.jpg",
         "image-large": "/desktop/image-deep-earth.jpg"
     },
     {
-        "name": "Night Arcade",
+        "label": "Night Arcade",
         "image-small": "/mobile/image-night-arcade.jpg",
         "image-large": "/desktop/image-night-arcade.jpg"
     },
     {
-        "name": "Soccer Team VR",
+        "label": "Soccer Team VR",
         "image-small": "/mobile/image-soccer-team.jpg",
         "image-large": "/desktop/image-soccer-team.jpg"
     },
     {
-        "name": "The Grid",
+        "label": "The Grid",
         "image-small": "/mobile/image-grid.jpg",
         "image-large": "/desktop/image-grid.jpg"
     },
     {
-        "name": "From Up Above VR",
+        "label": "From Up Above VR",
         "image-small": "/mobile/image-from-above.jpg",
         "image-large": "/desktop/image-from-above.jpg"
     },
     {
-        "name": "Pocket Borealis",
+        "label": "Pocket Borealis",
         "image-small": "/mobile/image-pocket-borealis.jpg",
         "image-large": "/desktop/image-pocket-borealis.jpg"
     },
     {
-        "name": "The Curiocity",
+        "label": "The Curiocity",
         "image-small": "/mobile/image-curiosity.jpg",
-        "image-large": "/desktop//image-curiosity.jpg"
+        "image-large": "/desktop/image-curiosity.jpg"
     },
     {
-        "name": "Make It Fisheye",
+        "label": "Make It Fisheye",
         "image-small": "/mobile/image-fisheye.jpg",
         "image-large": "/desktop/image-fisheye.jpg"
     }
