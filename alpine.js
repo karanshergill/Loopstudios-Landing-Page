b2ba8201a5e450edb0a333829193c2659ddad76a
Karan <email@karanshergill.com>
2024-10-17 12:25:30 +0530

    add responsive nav

--- alpine.js
+++ alpine.js
@@ -1,12 +1,13 @@
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
 import { navItems, creationItems } from './data.js';
+import 'boxicons';
 
 document.addEventListener('alpine:init', () => {
     console.log('Alpine is initialized');
 
     Alpine.data('alpineData', () => ({
         navItems,
-        isOpen: false,
+        isMobileMenuOpen: false,
         creationItems
     }));
 });
